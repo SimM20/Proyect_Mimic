a4c011616a7a52c5dfd25d6d91481a55b493ccd7
SimM20 <83238073+SimM20@users.noreply.github.com>
2025-03-31 22:48:36 -0300

    aa

--- Assets/Scripts/TouchHandler.cs
+++ Assets/Scripts/TouchHandler.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class TouchHandler : MonoBehaviour
 {
+    public static TouchHandler Instance;
+    public Action<Vector3> OnTouch;
+
+    private void Awake() 
+    {
+        if (Instance != null && Instance != this) Destroy(this);
+        else Instance = this;
+        EnhancedTouchSupport.Enable();
+    }
+
+    private void OnDisable() { EnhancedTouchSupport.Disable(); }
+
     private void Update()
     {
-        if (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed)
+        if (Touch.activeTouches.Count > 0)
         {
-            Vector2 touchPos = Touchscreen.current.primaryTouch.position.ReadValue();
-            Vector3 worldPos = Camera.main.ScreenToWorldPoint(touchPos);
-            Debug.Log("Touch: "+ worldPos);
+            Touch activeTouch = Touch.activeTouches[0];
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(activeTouch.screenPosition.x, activeTouch.screenPosition.y, Camera.main.nearClipPlane));
+            OnTouch?.Invoke(worldPos);
         }
     }
 }
